8131fb216734075a0be489f8ab4ae5385de7fb3f
Martin Thomson <mt@lowentropy.net>
2023-03-16 12:18:14 +1100

    Bump MSRV to 1.65.0 (#1424)
    * Update MSRV to 1.65
    
    Closes #1421.
    
    * Lots of clippy fixes
    
    * Drop workaround

--- src/agent.rs
+++ src/agent.rs
@@ -1048,15 +1048,11 @@ impl Server {
         for n in certificates {
             let c = CString::new(n.as_ref())?;
             let cert_ptr = unsafe { p11::PK11_FindCertFromNickname(c.as_ptr(), null_mut()) };
-            let cert = if let Ok(c) = unsafe { p11::Certificate::from_ptr(cert_ptr) } {
-                c
-            } else {
+            let Ok(cert) = (unsafe { p11::Certificate::from_ptr(cert_ptr) }) else {
                 return Err(Error::CertificateLoading);
             };
             let key_ptr = unsafe { p11::PK11_FindKeyByAnyCert(*cert.deref(), null_mut()) };
-            let key = if let Ok(k) = unsafe { p11::PrivateKey::from_ptr(key_ptr) } {
-                k
-            } else {
+            let Ok(key) = (unsafe { p11::PrivateKey::from_ptr(key_ptr) }) else {
                 return Err(Error::CertificateLoading);
             };
             secstatus_to_res(unsafe {
