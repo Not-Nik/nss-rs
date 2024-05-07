204ac92edd2e1f2bbd7103a3b20c7c514fcefce2
Benjamin Beurdouche <beurdouche@mozilla.com>
2024-05-07 14:54:07 +0200

    Support EC keygen for Ed25519 and refactor

--- src/ec.rs
+++ src/ec.rs
@@ -6,7 +6,9 @@
 
 use std::ptr;
 
-use pkcs11_bindings::{CKF_DERIVE, CKM_EC_KEY_PAIR_GEN};
+use pkcs11_bindings::{
+    CKF_DERIVE, CKM_EC_EDWARDS_KEY_PAIR_GEN, CKM_EC_KEY_PAIR_GEN, CKM_EC_MONTGOMERY_KEY_PAIR_GEN,
+};
 
 use crate::{
     der,
@@ -24,10 +26,11 @@ use crate::{
 //
 
 pub enum EcCurve {
-    CurveP256,
-    CurveP384,
-    CurveP521,
-    Curve25519,
+    P256,
+    P384,
+    P521,
+    X25519,
+    Ed25519,
 }
 
 // Object identifiers in DER tag-length-value form
@@ -54,33 +57,27 @@ pub const OID_RS256_BYTES: &[u8] = &[
     0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d, 0x01, 0x01, 0x0b,
 ];
 
-pub const OID_CURVE25519_BYTES: &[u8] = &[
+pub const OID_X25519_BYTES: &[u8] = &[
     /* https://tools.ietf.org/html/draft-josefsson-pkix-newcurves-01
      * 1.3.6.1.4.1.11591.15.1 */
     0x2b, 0x06, 0x01, 0x04, 0x01, 0xDA, 0x47, 0x0F, 0x01,
 ];
 
-// /*
-// 	https://oid-rep.orange-labs.fr/get/1.3.101.112
-// 	A.1.  ASN.1 Object for Ed25519
-// 	id-Ed25519 OBJECT IDENTIFIER ::= { 1.3.101.112 }
-// 	Parameters are absent.  Length is 7 bytes.
-// 	Binary encoding: 3005 0603 2B65 70
+pub const OID_ED35519_BYTES: &[u8] = &[
+    /*
+        https://oid-rep.orange-labs.fr/get/1.3.101.112
+        A.1.  ASN.1 Object for Ed25519
+        id-Ed25519 OBJECT IDENTIFIER ::= { 1.3.101.112 }
+        Parameters are absent.  Length is 7 bytes.
+        Binary encoding: 3005 0603 2B65 70
 
-// 	The same algorithm identifiers are used for identifying a public key,
-// 	a private key, and a signature (for the two EdDSA related OIDs).
-// 	Additional encoding information is provided below for each of these
-// 	locations.
-// */
-// CONST_OID ed25519PublicKey[] = { 0x2B, 0x65, 0x70 };
-// CONST_OID ed25519Signature[] = { 0x2B, 0x65, 0x70 };
-
-// fn ec_curve_to_ckm(alg: &EcCurve) -> p11::CK_MECHANISM_TYPE {
-//     match alg {
-//         EcCurve::CurveP256 => p11::CKM_EC_KEY_PAIR_GEN.into(),
-//         &EcCurve::CurveP384 | &EcCurve::CurveP521 | &EcCurve::Curve25519 => todo!(),
-//     }
-// }
+        The same algorithm identifiers are used for identifying a public key,
+        a private key, and a signature (for the two EdDSA related OIDs).
+        Additional encoding information is provided below for each of these
+        locations.
+    */
+    0x2B, 0x65, 0x70,
+];
 
 pub fn object_id(val: &[u8]) -> Result<Vec<u8>, crate::Error> {
     let mut out = Vec::with_capacity(der::MAX_TAG_AND_LENGTH_BYTES + val.len());
@@ -91,10 +88,19 @@ pub fn object_id(val: &[u8]) -> Result<Vec<u8>, crate::Error> {
 
 fn ec_curve_to_oid(alg: &EcCurve) -> Vec<u8> {
     match alg {
-        EcCurve::Curve25519 => OID_CURVE25519_BYTES.to_vec(),
-        EcCurve::CurveP256 => OID_SECP256R1_BYTES.to_vec(),
-        EcCurve::CurveP384 => OID_SECP384R1_BYTES.to_vec(),
-        EcCurve::CurveP521 => OID_SECP521R1_BYTES.to_vec(),
+        EcCurve::X25519 => OID_X25519_BYTES.to_vec(),
+        EcCurve::Ed25519 => OID_ED25519_BYTES.to_vec(),
+        EcCurve::P256 => OID_SECP256R1_BYTES.to_vec(),
+        EcCurve::P384 => OID_SECP384R1_BYTES.to_vec(),
+        EcCurve::P521 => OID_SECP521R1_BYTES.to_vec(),
+    }
+}
+
+const fn ec_curve_to_ckm(alg: &EcCurve) -> pkcs11_bindings::CK_MECHANISM_TYPE {
+    match alg {
+        EcCurve::P256 | EcCurve::P384 | EcCurve::P521 => CKM_EC_KEY_PAIR_GEN,
+        EcCurve::Ed25519 => CKM_EC_EDWARDS_KEY_PAIR_GEN,
+        EcCurve::X25519 => CKM_EC_MONTGOMERY_KEY_PAIR_GEN,
     }
 }
 
@@ -111,6 +117,9 @@ pub fn keygen(alg: EcCurve) -> Result<(PrivateKey, PublicKey), crate::Error> {
     let mut oid = SECItemBorrowed::wrap(&oid_bytes)?;
     let oid_ptr: *mut SECItem = oid.as_mut();
 
+    // Get the Mechanism based on the Curve and its use
+    let ckm = ec_curve_to_ckm(&alg);
+
     let slot = Slot::internal()?;
 
     let mut client_public_ptr = ptr::null_mut();
@@ -122,7 +131,7 @@ pub fn keygen(alg: EcCurve) -> Result<(PrivateKey, PublicKey), crate::Error> {
             // `SECKEYECParams *` which is a typedef for `SECItem *`.
             PK11_GenerateKeyPairWithOpFlags(
                 *slot,
-                CKM_EC_KEY_PAIR_GEN,
+                ckm,
                 oid_ptr.cast(),
                 &mut client_public_ptr,
                 PK11_ATTR_EXTRACTABLE | PK11_ATTR_INSENSITIVE | PK11_ATTR_SESSION,
