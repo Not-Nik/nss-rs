1a9301f568e899a5050625df4a80f4f1e9272198
Lars Eggert <lars@eggert.org>
2025-01-10 16:40:29 +0200

    Inline format items in log calls
    ---------
    
    Signed-off-by: Lars Eggert <lars@eggert.org>

--- src/ech.rs
+++ src/ech.rs
@@ -145,7 +145,7 @@ pub fn generate_keys() -> Res<(PrivateKey, PublicKey)> {
     assert_eq!(secret_ptr.is_null(), public_ptr.is_null());
     let sk = unsafe { PrivateKey::from_ptr(secret_ptr) }?;
     let pk = unsafe { PublicKey::from_ptr(public_ptr) }?;
-    trace!("Generated key pair: sk={:?} pk={:?}", sk, pk);
+    trace!("Generated key pair: sk={sk:?} pk={pk:?}");
     Ok((sk, pk))
 }
 
