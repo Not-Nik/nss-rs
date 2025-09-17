mozilla/neqo

More lints (#1436)
* Remove let from _ bindings

This is the preferred way to ignore a return value now, apparently.

The alternative is `let _: ReturnType =`, which is nice in the sense
that it ensures that the type remains stable, but that is verbose and
mostly these are just in test code.

* Fix a few lints

neqo-crypto/tests/hp.rs
@@ -1,12 +1,14 @@
 #![cfg_attr(feature = "deny-warnings", deny(warnings))]
 #![warn(clippy::pedantic)]
 
-use neqo_crypto::constants::{
-    Cipher, TLS_AES_128_GCM_SHA256, TLS_AES_256_GCM_SHA384, TLS_CHACHA20_POLY1305_SHA256,
-    TLS_VERSION_1_3,
+use neqo_crypto::{
+    constants::{
+        Cipher, TLS_AES_128_GCM_SHA256, TLS_AES_256_GCM_SHA384, TLS_CHACHA20_POLY1305_SHA256,
+        TLS_VERSION_1_3,
+    },
+    hkdf,
+    hp::HpKey,
 };
-use neqo_crypto::hkdf;
-use neqo_crypto::hp::HpKey;
 use std::mem;
 use test_fixture::fixture_init;
 
@@ -22,6 +24,7 @@ fn hp_test(cipher: Cipher, expected: &[u8]) {
     let mask = hp.mask(&[0; 16]).expect("should produce a mask");
     assert_eq!(mask, expected, "first invocation should be correct");
 
+    #[allow(clippy::redundant_clone)] // This is deliberate.
     let hp2 = hp.clone();
     let mask = hp2.mask(&[0; 16]).expect("clone produces mask");
     assert_eq!(mask, expected, "clone should produce the same mask");

Not-Nik/nss-rs

tests/hp.rs
@@ -1,12 +1,14 @@
 #![cfg_attr(feature = "deny-warnings", deny(warnings))]
 #![warn(clippy::pedantic)]
 
-use nss_gk_api::constants::{
-    Cipher, TLS_AES_128_GCM_SHA256, TLS_AES_256_GCM_SHA384, TLS_CHACHA20_POLY1305_SHA256,
-    TLS_VERSION_1_3,
+use nss_gk_api::{
+    constants::{
+        Cipher, TLS_AES_128_GCM_SHA256, TLS_AES_256_GCM_SHA384, TLS_CHACHA20_POLY1305_SHA256,
+        TLS_VERSION_1_3,
+    },
+    hkdf,
+    hp::HpKey,
 };
-use nss_gk_api::hkdf;
-use nss_gk_api::hp::HpKey;
 use std::mem;
 use test_fixture::fixture_init;
 
@@ -22,6 +24,7 @@ fn hp_test(cipher: Cipher, expected: &[u8]) {
     let mask = hp.mask(&[0; 16]).expect("should produce a mask");
     assert_eq!(mask, expected, "first invocation should be correct");
 
+    #[allow(clippy::redundant_clone)] // This is deliberate.
     let hp2 = hp.clone();
     let mask = hp2.mask(&[0; 16]).expect("clone produces mask");
     assert_eq!(mask, expected, "clone should produce the same mask");