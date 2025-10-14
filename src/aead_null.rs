mozilla/neqo

fix: Implement a trait for `Aead` (#3014)

neqo-crypto/src/aead_null.rs
@@ -4,11 +4,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![expect(clippy::missing_errors_doc, reason = "OK here.")]
-
 use std::fmt;
 
 use crate::{
+    aead::Aead,
     constants::{Cipher, Version},
     err::{sec::SEC_ERROR_BAD_DATA, Error, Res},
     p11::SymKey,
@@ -19,70 +18,14 @@ pub const AEAD_NULL_TAG: &[u8] = &[0x0a; 16];
 pub struct AeadNull {}
 
 impl AeadNull {
-    #[expect(
-        clippy::unnecessary_wraps,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
-    pub const fn new(
-        _version: Version,
-        _cipher: Cipher,
-        _secret: &SymKey,
-        _prefix: &str,
-    ) -> Res<Self> {
-        Ok(Self {})
-    }
-
-    #[must_use]
-    pub const fn expansion() -> usize {
-        AEAD_NULL_TAG.len()
-    }
-
-    #[expect(
-        clippy::unnecessary_wraps,
-        clippy::unused_self,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
-    pub fn encrypt<'a>(
-        &self,
-        _count: u64,
-        _aad: &[u8],
-        input: &[u8],
-        output: &'a mut [u8],
-    ) -> Res<&'a [u8]> {
-        let l = input.len();
-        output[..l].copy_from_slice(input);
-        output[l..l + Self::expansion()].copy_from_slice(AEAD_NULL_TAG);
-        Ok(&output[..l + Self::expansion()])
-    }
-
-    #[expect(
-        clippy::unnecessary_wraps,
-        clippy::unused_self,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
-    pub fn encrypt_in_place<'a>(
-        &self,
-        _count: u64,
-        _aad: &[u8],
-        data: &'a mut [u8],
-    ) -> Res<&'a mut [u8]> {
-        let pos = data.len() - Self::expansion();
-        data[pos..].copy_from_slice(AEAD_NULL_TAG);
-        Ok(data)
-    }
-
-    #[expect(
-        clippy::unused_self,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
     fn decrypt_check(&self, _count: u64, _aad: &[u8], input: &[u8]) -> Res<usize> {
-        if input.len() < Self::expansion() {
+        if input.len() < self.expansion() {
             return Err(Error::from(SEC_ERROR_BAD_DATA));
         }
 
         let len_encrypted = input
             .len()
-            .checked_sub(Self::expansion())
+            .checked_sub(self.expansion())
             .ok_or_else(|| Error::from(SEC_ERROR_BAD_DATA))?;
         // Check that:
         // 1) expansion is all zeros and
@@ -96,8 +39,42 @@ impl AeadNull {
             Err(Error::from(SEC_ERROR_BAD_DATA))
         }
     }
+}
 
-    pub fn decrypt<'a>(
+impl Aead for AeadNull {
+    fn new(_version: Version, _cipher: Cipher, _secret: &SymKey, _prefix: &str) -> Res<Self> {
+        Ok(Self {})
+    }
+
+    fn expansion(&self) -> usize {
+        AEAD_NULL_TAG.len()
+    }
+
+    fn encrypt<'a>(
+        &self,
+        _count: u64,
+        _aad: &[u8],
+        input: &[u8],
+        output: &'a mut [u8],
+    ) -> Res<&'a [u8]> {
+        let l = input.len();
+        output[..l].copy_from_slice(input);
+        output[l..l + self.expansion()].copy_from_slice(AEAD_NULL_TAG);
+        Ok(&output[..l + self.expansion()])
+    }
+
+    fn encrypt_in_place<'a>(
+        &self,
+        _count: u64,
+        _aad: &[u8],
+        data: &'a mut [u8],
+    ) -> Res<&'a mut [u8]> {
+        let pos = data.len() - self.expansion();
+        data[pos..].copy_from_slice(AEAD_NULL_TAG);
+        Ok(data)
+    }
+
+    fn decrypt<'a>(
         &self,
         count: u64,
         aad: &[u8],
@@ -110,7 +87,7 @@ impl AeadNull {
         })
     }
 
-    pub fn decrypt_in_place<'a>(
+    fn decrypt_in_place<'a>(
         &self,
         count: u64,
         aad: &[u8],

neqo-crypto/src/selfencrypt.rs
@@ -9,6 +9,7 @@ use std::mem;
 use neqo_common::{hex, qinfo, qtrace, Encoder};
 
 use crate::{
+    aead::Aead as _,
     constants::{Cipher, Version},
     err::{Error, Res},
     hkdf,
@@ -84,7 +85,7 @@ impl SelfEncrypt {
         // AAD covers the entire header, plus the value of the AAD parameter that is provided.
         let salt = random::<{ Self::SALT_LENGTH }>();
         let cipher = self.make_aead(&self.key, &salt)?;
-        let encoded_len = 2 + salt.len() + plaintext.len() + Aead::expansion();
+        let encoded_len = 2 + salt.len() + plaintext.len() + cipher.expansion();
 
         let mut enc = Encoder::with_capacity(encoded_len);
         enc.encode_byte(Self::VERSION);

Not-Nik/nss-rs

src/aead_null.rs
@@ -4,11 +4,10 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![expect(clippy::missing_errors_doc, reason = "OK here.")]
-
 use std::fmt;
 
 use crate::{
+    aead::AeadTrait,
     constants::{Cipher, Version},
     err::{sec::SEC_ERROR_BAD_DATA, Error, Res},
     p11::SymKey,
@@ -19,70 +18,14 @@ pub const AEAD_NULL_TAG: &[u8] = &[0x0a; 16];
 pub struct AeadNull {}
 
 impl AeadNull {
-    #[expect(
-        clippy::unnecessary_wraps,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
-    pub const fn new(
-        _version: Version,
-        _cipher: Cipher,
-        _secret: &SymKey,
-        _prefix: &str,
-    ) -> Res<Self> {
-        Ok(Self {})
-    }
-
-    #[must_use]
-    pub const fn expansion() -> usize {
-        AEAD_NULL_TAG.len()
-    }
-
-    #[expect(
-        clippy::unnecessary_wraps,
-        clippy::unused_self,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
-    pub fn encrypt<'a>(
-        &self,
-        _count: u64,
-        _aad: &[u8],
-        input: &[u8],
-        output: &'a mut [u8],
-    ) -> Res<&'a [u8]> {
-        let l = input.len();
-        output[..l].copy_from_slice(input);
-        output[l..l + Self::expansion()].copy_from_slice(AEAD_NULL_TAG);
-        Ok(&output[..l + Self::expansion()])
-    }
-
-    #[expect(
-        clippy::unnecessary_wraps,
-        clippy::unused_self,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
-    pub fn encrypt_in_place<'a>(
-        &self,
-        _count: u64,
-        _aad: &[u8],
-        data: &'a mut [u8],
-    ) -> Res<&'a mut [u8]> {
-        let pos = data.len() - Self::expansion();
-        data[pos..].copy_from_slice(AEAD_NULL_TAG);
-        Ok(data)
-    }
-
-    #[expect(
-        clippy::unused_self,
-        reason = "Need to replicate the API of aead::RealAead."
-    )]
     fn decrypt_check(&self, _count: u64, _aad: &[u8], input: &[u8]) -> Res<usize> {
-        if input.len() < Self::expansion() {
+        if input.len() < self.expansion() {
             return Err(Error::from(SEC_ERROR_BAD_DATA));
         }
 
         let len_encrypted = input
             .len()
-            .checked_sub(Self::expansion())
+            .checked_sub(self.expansion())
             .ok_or_else(|| Error::from(SEC_ERROR_BAD_DATA))?;
         // Check that:
         // 1) expansion is all zeros and
@@ -96,8 +39,42 @@ impl AeadNull {
             Err(Error::from(SEC_ERROR_BAD_DATA))
         }
     }
+}
 
-    pub fn decrypt<'a>(
+impl AeadTrait for AeadNull {
+    fn new(_version: Version, _cipher: Cipher, _secret: &SymKey, _prefix: &str) -> Res<Self> {
+        Ok(Self {})
+    }
+
+    fn expansion(&self) -> usize {
+        AEAD_NULL_TAG.len()
+    }
+
+    fn encrypt<'a>(
+        &self,
+        _count: u64,
+        _aad: &[u8],
+        input: &[u8],
+        output: &'a mut [u8],
+    ) -> Res<&'a [u8]> {
+        let l = input.len();
+        output[..l].copy_from_slice(input);
+        output[l..l + self.expansion()].copy_from_slice(AEAD_NULL_TAG);
+        Ok(&output[..l + self.expansion()])
+    }
+
+    fn encrypt_in_place<'a>(
+        &self,
+        _count: u64,
+        _aad: &[u8],
+        data: &'a mut [u8],
+    ) -> Res<&'a mut [u8]> {
+        let pos = data.len() - self.expansion();
+        data[pos..].copy_from_slice(AEAD_NULL_TAG);
+        Ok(data)
+    }
+
+    fn decrypt<'a>(
         &self,
         count: u64,
         aad: &[u8],
@@ -110,7 +87,7 @@ impl AeadNull {
         })
     }
 
-    pub fn decrypt_in_place<'a>(
+    fn decrypt_in_place<'a>(
         &self,
         count: u64,
         aad: &[u8],

src/selfencrypt.rs
@@ -9,6 +9,7 @@ use std::{fmt::Write as _, io::Write as _, mem};
 use log::{info, trace};
 
 use crate::{
+    aead::AeadTrait as _,
     constants::{Cipher, Version},
     err::{Error, Res},
     hkdf,
@@ -93,7 +94,7 @@ impl SelfEncrypt {
         // AAD covers the entire header, plus the value of the AAD parameter that is provided.
         let salt = random::<{ Self::SALT_LENGTH }>();
         let cipher = self.make_aead(&self.key, &salt)?;
-        let encoded_len = 2 + salt.len() + plaintext.len() + Aead::expansion();
+        let encoded_len = 2 + salt.len() + plaintext.len() + cipher.expansion();
 
         let mut enc = Vec::<u8>::with_capacity(encoded_len);
         enc.write_all(&[Self::VERSION])